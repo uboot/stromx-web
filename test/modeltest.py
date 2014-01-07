d48b70bf4d350757521d7b746317ff49677dd586
Matthias Fuchs <matz.fuchs@gmx.at>
2014-01-07 21:15:32 +0100

    Add first (failing) unit test

--- test/modeltest.py
+++ test/modeltest.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
+import model
+
+class FilesTest(unittest.TestCase):
+    def setUp(self):
+        self.__files = model.Files("../files")
+
+    def testData(self):
+        self.assertEqual("", self.__files.data)
+        
+if __name__ == '__main__':
+    unittest.main()
