413c9eeeda91c27b97bc723f840a45d1377071c2
Matthias Fuchs <matz.fuchs@gmx.at>
2014-01-08 09:17:16 +0100

    Setup test data

--- test/modeltest.py
+++ test/modeltest.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 
+import shutil
 import unittest
 
 import model
 
 class FilesTest(unittest.TestCase):
     def setUp(self):
-        self.__files = model.Files("../files")
+        shutil.rmtree("temp", True)
+        shutil.copytree("data", "temp")
+            
+        self.__files = model.Files("temp")
 
     def testData(self):
         self.assertEqual("", self.__files.data)
+        
+    def tearDown(self):
+        shutil.rmtree("temp", True)
         
 if __name__ == '__main__':
     unittest.main()
