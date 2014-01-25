7392e34ec06e9d77a88e40eaf972123b4803657d
Matthias Fuchs <matz.fuchs@gmx.at>
2014-01-25 22:12:18 +0100

    Move application controller to dedicate file

--- static/js/controllers/error_controller.js
+++ static/js/controllers/error_controller.js
@@ -5,24 +5,3 @@ App.ErrorController = Ember.ObjectController.extend({
     return time.toLocaleTimeString()
   }.property('model.time')
 });
-
-App.ApplicationController = Ember.ArrayController.extend({
-  init: function() {
-    this._super()
-    var url = 'ws://' + window.location.host + '/error_socket'
-    var ws = new WebSocket(url);
-    var store = this.store
-    var controller = this
-    ws.onmessage = function(event) {
-      var payload = JSON.parse(event.data)
-      store.pushPayload('error', payload)
-      Ember.run.next(function() {
-        store.find('error', payload.error.id).then(function(error) {
-          controller.pushObject(error)
-        })
-      })
-    }
-  },
-  sortProperties: ['time'],
-  sortAscending: false
-});
