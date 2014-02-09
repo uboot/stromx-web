7e39d53fd489f401236fb04790b1d79786ad7782
Matthias Fuchs <matz.fuchs@gmx.at>
2014-02-09 18:23:40 +0100

    Show enum title instead of number

--- static/js/controllers/parameter_controller.js
+++ static/js/controllers/parameter_controller.js
@@ -1,13 +1,33 @@
 App.ParameterController = Ember.ObjectController.extend({  
-  value: function() {
+  value: function(key, value) {
+    if (arguments.length > 1) {
+      return value
+    }
     var type = this.get('type')
     if (type == 'int')
       return this.get('valueNumber')
     else if (type == 'string')
       return this.get('valueString')
     else if (type == 'enum')
-      return this.get('valueNumber')
+      return this.updateEnumTitle(this.get('valueNumber'))
     else
       return ''
-  }.property('valueString', 'valueNumber', 'type')
+  }.property('valueString', 'valueNumber', 'type'),
+  
+  updateEnumTitle: function(enumValue) {
+    var title = this.get('descriptions').then( function(value){
+      return value.find( function(item, index, enumerable) {
+         return item.get('value') == enumValue
+      })
+    }).then( function(obj) {
+      return obj.get('title')
+    })
+    
+    that = this
+    title.then( function(title) {
+      that.set('value', title)
+    })
+    
+    return enumValue
+  }
 });
