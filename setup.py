8e88ae535535c8794fa4a190a1afb5b7a2ca007b
Matthias Fuchs <matz.fuchs@gmx.at>
2014-05-07 21:20:02 +0200

    Fix server with REST adapter, move everything to package stromxweb

--- setup.py
+++ setup.py
@@ -8,5 +8,7 @@ setup(name='stromx-web',
       author='Matthias Fuchs',
       author_email='stromx-devel@googlegroups.com',
       url='http://www.stromx.org',
-      py_modules=['model', 'server'],
+      packages=['stromxweb'],
+      package_data={'stromxweb': ['static/*']},
+      requires=['tornado(>=1.0)']
 )
