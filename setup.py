5195b8132d41561bb85111a8a42053d774164d62
Matthias Fuchs <matz.fuchs@gmx.at>
2014-05-12 21:26:19 +0200

    Include all static files in setup

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
+import os
 
+static_files = []
+
+for root, dirs, files in os.walk('stromxweb/static', ):
+    relative_root = os.path.relpath(root, 'stromxweb')
+    paths = [os.path.join(relative_root, file) for file in files]
+    static_files.extend(paths)
+
+print static_files
 setup(name='stromx-web',
       version='0.1',
       description='Web interface to operate stromx streams',
@@ -9,13 +18,6 @@ setup(name='stromx-web',
       author_email='stromx-devel@googlegroups.com',
       url='http://www.stromx.org',
       packages=['stromxweb'],
-      package_data={'stromxweb': ['static/*',
-                                  'static/css/*',
-                                  'static/fonts/*',
-                                  'static/js/*',
-                                  'static/js/controllers/*',
-                                  'static/js/libs/*',
-                                  'static/js/models/*',
-                                  'static/js/views/*']},
+      package_data={'stromxweb': static_files},
       requires=['tornado(>=3.0)']
 )
