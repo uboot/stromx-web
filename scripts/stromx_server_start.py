e9f443cd964f7576c361e64ab59ac691623175ae
Matthias Fuchs <matz.fuchs@gmx.at>
2014-05-14 22:07:04 +0200

    Start server without in daemon in start server script

--- scripts/stromx_server_start.py
+++ scripts/stromx_server_start.py
@@ -1,28 +1,11 @@
 #!/usr/bin/env python
 
 import argparse
-import daemon
-import os.path
-import signal
-
 import stromxweb
-
-def stopServer(signum, frame):
-    stromxweb.stop()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('directory', help=('the directory in which the stromx '
                                        'stream files will be stored'))
 args = parser.parse_args()
 
-files = os.path.abspath(args.directory)
-context = daemon.DaemonContext(
-    working_directory = files,
-    signal_map = { signal.SIGTERM: stopServer }
-)
-
-print 'Start stromx web server...'
-print 'Files: {0}'.format(files)
-
-with context:
-    stromxweb.start('.')
+stromxweb.start(args.directory)
