c18e09b2c60a6f985107d0ec28fd6f18de9ed506
Matthias Fuchs <matz.fuchs@gmx.at>
2014-06-10 21:59:18 +0200

    First version of operator moving

--- client/app/scripts/views/scene_operator_view.js
+++ client/app/scripts/views/scene_operator_view.js
@@ -25,6 +25,10 @@ App.SceneOperatorView = Ember.View.extend({
       class: 'stromx-svg-operator-rect'
     });
     group.add(opName, opRect);
+    var that = this;
+    group.drag(function(dx, dy){
+      that.move(dx, dy);
+    }, function(){}, function(){});
   },
   
   willDestroyElement: function() {
@@ -32,5 +36,14 @@ App.SceneOperatorView = Ember.View.extend({
     if (group)
       group.remove();
     this.set('group', null);
+  },
+  
+  move: function(dx, dy){
+    group = this.get('group');
+    opX = this.content.get('x');
+    opY = this.content.get('y');
+    group.attr({
+      transform: new Snap.Matrix().translate(opX + dx, opY + dy)
+    });
   }
 });
