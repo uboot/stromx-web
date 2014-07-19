798b9a9c40acd54580e90d33f82e9442329e890e
Matthias Fuchs <matz.fuchs@gmx.at>
2014-07-19 08:32:59 +0200

    Serialize many-to-one relations in fixture adapter

--- client/app/scripts/store.js
+++ client/app/scripts/store.js
@@ -4,3 +4,21 @@
 //   host: "http://localhost:8888"
 // });
 App.ApplicationAdapter = DS.FixtureAdapter;
+
+// TODO: cf. http://discuss.emberjs.com/t/ember-data-fixture-adapter-saving-record-loses-has-many-relationships/2821/3
+DS.JSONSerializer.reopen({
+  serializeHasMany : function(record, json, relationship) {
+    var key = relationship.key;
+
+    var relationshipType = DS.RelationshipChange.determineRelationshipType(
+      record.constructor, relationship);
+
+    if (relationshipType === 'manyToNone'
+        || relationshipType === 'manyToMany'
+        || relationshipType === 'manyToOne') {
+      json[key] = Ember.get(record, key).mapBy('id');
+      // TODO support for polymorphic manyToNone and manyToMany
+      // relationships
+    }
+  }
+});
