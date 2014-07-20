22b5fa25b1d4b007b4bbde180d70ba8946845a38
Matthias Fuchs <matz.fuchs@gmx.at>
2014-07-20 21:45:38 +0200

    Use common field for all parameter values

--- client/app/scripts/controllers/parameter_controller.js
+++ client/app/scripts/controllers/parameter_controller.js
@@ -1,60 +1,60 @@
 /* global App */
 
-App.ParameterController = Ember.ObjectController.extend({  
+App.ParameterController = Ember.ObjectController.extend({
   isEditing: false,
-  
+
   isEnum: function() {
-    return this.get('type') === 'enum';
-  }.property('type'),
-  
+    return this.get('variant') === 'enum';
+  }.property('variant'),
+
   isString: function() {
-    return this.get('type') === 'string';
-  }.property('type'),
-  
+    return this.get('variant') === 'string';
+  }.property('variant'),
+
   isInt: function() {
-    return this.get('type') === 'int';
-  }.property('type'),
-  
+    return this.get('variant') === 'int';
+  }.property('variant'),
+
   isFloat: function() {
-    return this.get('type') === 'float';
-  }.property('type'),
-  
+    return this.get('variant') === 'float';
+  }.property('variant'),
+
   isBool: function() {
-    return this.get('type') === 'bool';
-  }.property('type'),
-  
+    return this.get('variant') === 'bool';
+  }.property('variant'),
+
   isTrigger: function() {
-    return this.get('type') === 'trigger';
-  }.property('type'),
-  
+    return this.get('variant') === 'trigger';
+  }.property('variant'),
+
   timedOut: function() {
     return this.get('state') === 'timedOut';
   }.property('state'),
-  
+
   current: function() {
     return this.get('state') === 'current';
-  }.property('state'),                                                        
-                                                        
+  }.property('state'),
+
   editable: function() {
-    var typeIsKnown = (this.get('isString') || 
-                       this.get('isEnum') || 
-                       this.get('isInt') || 
+    var typeIsKnown = (this.get('isString') ||
+                       this.get('isEnum') ||
+                       this.get('isInt') ||
                        this.get('isFloat'));
     return this.get('writable') && this.get('current') && typeIsKnown;
   }.property('writable', 'isString', 'isInt', 'isFloat', 'state'),
-                                                        
+
   writeOnly: Ember.computed.not('writable'),
-                                                        
+
   accessFailed: function() {
     return this.get('state') === 'accessFailed';
   }.property('state'),
-                                                        
+
   editValue:  function(key, value) {
     if (value === undefined) {
       if (this.get('isInt') || this.get('isFloat'))
-        return this.get('numberValue');
+        return this.get('value');
       else if (this.get('isString'))
-        return this.get('stringValue');
+        return this.get('value');
       else
         return '';
     }
@@ -62,55 +62,56 @@ App.ParameterController = Ember.ObjectController.extend({
       var v;
       if (this.get('isInt')) {
         v = parseInt(value, 10);
-        this.set('numberValue', v);
+        this.set('value', v);
         return v;
       }
       else if (this.get('isFloat')) {
         v = parseFloat(value);
-        this.set('numberValue', v);
+        this.set('value', v);
         return v;
       }
       else if (this.get('isString')) {
-        this.set('stringValue', value);
+        this.set('value', value);
         return value;
       }
     }
-  }.property('stringValue', 'numberValue', 'type'),      
-                                                        
+  }.property('value', 'variant'),
+
   boolValue:  function(key, value) {
     if (value === undefined) {
-      return this.get('numberValue') === 1;
+      return this.get('value');
     }
     else {
-      var v = value ? 1 : 0;
-      this.set('numberValue', v);
+      this.set('value', value);
       var model = this.get('model');
       model.save();
       return value;
     }
-  }.property('numberValue'),
-                                                        
+  }.property('value'),
+
   displayValue: function(key, value) {
     if (value !== undefined)
       return value;
-      
+
     if (! this.get('current'))
       return '';
-    
+
     if (this.get('isEditing'))
       return '';
-      
+
     if (this.get('isInt'))
-      return this.get('numberValue');
+      return this.get('value');
     else if (this.get('isFloat'))
-      return this.get('numberValue');
+      return this.get('value');
     else if (this.get('isEnum'))
-      return this.updateEnumTitle(this.get('numberValue'));
-    else 
-      return this.get('stringValue');
-      
-  }.property('stringValue', 'numberValue', 'type', 'isEditing'),   
-                                                        
+      return this.updateEnumTitle(this.get('value'));
+    else if (this.get('isBool'))
+      return "";
+    else
+      return this.get('value');
+
+  }.property('value', 'variant', 'isEditing'),
+
   // cf. http://stackoverflow.com/q/20623027
   updateEnumTitle: function(enumValue) {
     var value = enumValue;
@@ -121,32 +122,32 @@ App.ParameterController = Ember.ObjectController.extend({
     }).then( function(obj) {
       return obj.get('title');
     });
-    
+
     var that = this;
     title.then( function(title) {
       that.set('displayValue', title);
       value = title;
     });
-    
+
     return value;
   },
-  
+
   actions: {
     editValue: function() {
       this.set('isEditing', true);
-    }, 
-    
+    },
+
     saveValue: function() {
       this.set('isEditing', false);
       var model = this.get('model');
       model.save();
     },
-    
+
     reload: function() {
       var model = this.get('model');
       model.reload();
     },
-    
+
     trigger: function() {
       this.set('numberValue', 1);
       var model = this.get('model');
