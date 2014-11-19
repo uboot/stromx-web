a25aff98d1256b34dad2c21d6edf3eb88eb0603b
Matthias Fuchs <matz.fuchs@gmx.at>
2014-11-19 21:33:26 +0100

    Add comment

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -3,6 +3,7 @@ import DS from "ember-data";
 import ENV from '../config/environment';
 
 export default DS.RESTAdapter.extend({
+  // host: "http://localhost:8888",
   host: ENV.APP.API_HOST,
   namespace: 'api'
 });
