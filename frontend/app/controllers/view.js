4015828c17f553c9789850630c0d550df238e6ff
Matthias Fuchs <matz.fuchs@gmx.at>
2016-02-11 21:52:06 +0100

    Fix jshint warning

--- frontend/app/controllers/view.js
+++ frontend/app/controllers/view.js
@@ -19,7 +19,6 @@ export default Ember.Controller.extend({
       this.set('isEditingName', true);
     },
     updateSorting: function() {
-      var _this = this;
       var model = this.get('model');
       model.get('observers').forEach(function(observer) {
         observer.reload();
