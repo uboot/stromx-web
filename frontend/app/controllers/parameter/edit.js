46de996ac399c833635f1a55336caff3397fc777
Matthias Fuchs <matz.fuchs@gmx.at>
2016-08-11 21:32:49 +0200

    Fix cancelling matrix edit

--- frontend/app/controllers/parameter/edit.js
+++ frontend/app/controllers/parameter/edit.js
@@ -1,32 +1,41 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  currentValue: null,
-  modelChanged: Ember.observer('model', 'model.value', function() {
-    var value = this.get('model.value');
-    if (! value) {
-      var rows = this.get('model.rows') > 0 ? this.get('model.rows') : 1;
-      var cols = this.get('model.cols') > 0 ? this.get('model.cols') : 1;
-      var values = [];
-      for (var i = 0; i < rows; ++i) {
-        values.push(new Array(cols).fill(0));
+  value: Ember.computed('model.value', {
+    get: function() {
+      var value = this.get('model.value');
+      if (! value) {
+        var rows = this.get('model.rows') > 0 ? this.get('model.rows') : 1;
+        var cols = this.get('model.cols') > 0 ? this.get('model.cols') : 1;
+        var values = [];
+        for (var i = 0; i < rows; ++i) {
+          values.push(new Array(cols).fill(0));
+        }
+        var defaultValue = {
+          rows: rows,
+          cols: cols,
+          values: values
+        };
+        return defaultValue;
       }
-      var defaultValue = {
-        rows: rows,
-        cols: cols,
-        values: values
-      };
-      this.set('model.value', defaultValue);
+
+      return Ember.$.extend(true, {}, value);
+    },
+    set: function(key, value) {
+      this.set('model.value', Ember.$.extend(true, {}, value));
+      return value;
     }
   }),
   actions: {
-    dismiss: function () {
+    dismiss: function() {
       this.transitionToRoute('operator.index', this.get('model.operator'));
     },
-    cancel: function () {
+    cancel: function() {
+      this.set('value', null);
       this.get('model').rollbackAttributes();
     },
-    save: function () {
+    save: function(value) {
+      this.set('value', value);
       this.get('model').save();
     }
   }
