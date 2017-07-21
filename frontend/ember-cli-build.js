5fd4e4f1deb8ac6894ee3230a46679001fec2c47
Matthias Fuchs <matz.fuchs@gmx.at>
2017-07-21 21:27:41 +0200

    Use ember bootstrap

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -1,13 +1,16 @@
 /* eslint-env node */
-'use-strict';
+'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-bootstrap': {
+      'bootstrapVersion': 3,
+      'importBootstrapFont': true,
+      'importBootstrapCSS': false
+    }
   });
-
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
@@ -21,22 +24,6 @@ module.exports = function(defaults) {
   // modules that you would like to import into your application
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
-  app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.eot', {
-      destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.svg', {
-      destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.ttf', {
-      destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff', {
-      destDir: 'fonts'
-  });
-  app.import('bower_components/bootstrap/fonts/glyphicons-halflings-regular.woff2', {
-      destDir: 'fonts'
-  });
 
   return app.toTree();
 };
