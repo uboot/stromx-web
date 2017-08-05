7d3e4b71315a1ca3105d1d85a741a3f91cf50d36
Matthias Fuchs <matz.fuchs@gmx.at>
2017-08-05 16:35:09 +0200

    Do not build frontend in setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,6 @@
 from distutils.core import setup
 import os
 import shutil
-import subprocess
-
-# build the client
-os.chdir('frontend')
-if os.path.exists('dist'):
-    shutil.rmtree('dist')
-env = os.environ.copy()
-subprocess.call(['npm', 'install'])
-subprocess.call(['node', './node_modules/.bin/ember', 'build', '--environment=production'])
-os.chdir('..')
-
-# copy the client
-if os.path.exists('backend/stromxweb/static'):
-    shutil.rmtree('backend/stromxweb/static')
-shutil.copytree('frontend/dist', 'backend/stromxweb/static')
 
 # collect the client files
 static_files = []
